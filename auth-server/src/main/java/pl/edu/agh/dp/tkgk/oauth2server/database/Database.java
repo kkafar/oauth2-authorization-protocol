200506f4136bdb27162668501b896b685e5f4c1d
Gabriel Cyganek <400758@student.agh.edu.pl>
2021-12-27 21:58:45 +0100

    token revocation init

--- auth-server/src/main/java/pl/edu/agh/dp/tkgk/oauth2server/database/Database.java
+++ auth-server/src/main/java/pl/edu/agh/dp/tkgk/oauth2server/database/Database.java
@@ -2,6 +2,13 @@ package pl.edu.agh.dp.tkgk.oauth2server.database;
 
 public interface Database {
 
+    /**
+     * @return true if access token was revoked
+     */
+    boolean revokeAccessToken();
 
-
+    /**
+     * @return true if refresh token was revoked
+     */
+    boolean revokeRefreshToken();
 }
