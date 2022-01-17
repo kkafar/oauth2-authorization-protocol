90a56b6de9ba99eead75a302fb63bda6b3128cab
Kacper Kafara <kacperkafara@gmail.com>
2022-01-17 18:36:50 +0100

    Add simple assertion for number of arguments in test-server

--- test-server/src/main.py
+++ test-server/src/main.py
@@ -12,11 +12,13 @@ def echo():
 
 
 if __name__ == "__main__":
+    assert len(sys.argv) == 2, "Server mode name must be provided! 'debug' or 'dev'"
+    assert sys.argv[1] in {'debug', 'dev'}, "Server mode name must be provided! 'debug' or 'dev'"
+
     match sys.argv[1]:
         case "debug":
-            app.run(port=cfg.config['port'])
+            configuration = {'port': cfg.config['port']}
         case "dev":
-            app.run(port=cfg.config['port'], host=cfg.config['hostAddress'])
-        case _:
-            app.run(port=cfg.config['port'])
+            configuration = {'port': cfg.config['port'], 'host': cfg.config['hostAddress']}
             
+    app.run(**configuration)
