010bc4e16b3deb9eb03a1103e4253e95565c7401
Kacper Kafara <kacperkafara@gmail.com>
2022-02-13 17:52:59 +0100

    Adjust test-server impl

--- test-server/src/main.py
+++ test-server/src/main.py
@@ -5,9 +5,31 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def echo():
-    return "<p>Everybody hands up! We are going crazy!</p>"
+    return "<p>Response from server</p>"
+
+
+@app.route("/auth")
+def authorization_form():
+    return """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Login</title>
+        </head>
+        <body>
+        <form action="$SUBMIT_URL" method="post" enctype="application/x-www-form-urlencoded">
+            <label for="login">Login:</label>
+            <input type="text" id="login" name="login"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+        </body>
+        </html>"""
 
 
 @app.route("/data", methods=['POST', 'GET'])
