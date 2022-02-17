2d647d5cc8b6d13433be092a634b54b32f7dbfe1
Kacper Kafara <kacperkafara@gmail.com>
2022-02-17 21:13:29 +0100

    Add more specific queries

--- apps/clientapp/app/src/main/java/com/dp/database/dao/UserAuthInfoDao.java
+++ apps/clientapp/app/src/main/java/com/dp/database/dao/UserAuthInfoDao.java
@@ -16,8 +16,27 @@ public interface UserAuthInfoDao {
   @Query("SELECT * FROM UserAuthInfo")
   List<UserAuthInfo> getAll();
 
-  @Query("SELECT * FROM UserAuthInfo WHERE uid == :id")
-  UserAuthInfo findById(int id);
+  @Query("SELECT * FROM UserAuthInfo WHERE uid == :uid")
+  UserAuthInfo findById(int uid);
+
+  @Query("SELECT auth_code FROM UserAuthInfo WHERE uid == :uid")
+  String findAuthCode(int uid);
+
+  @Query("SELECT code_verifier FROM UserAuthInfo WHERE uid == :uid")
+  String findCodeVerifierById(int uid);
+
+  @Query("SELECT token FROM UserAuthInfo WHERE uid == :uid")
+  String findTokenByUid(int uid);
+
+  @Query("SELECT refresh_token FROM UserAuthInfo WHERE uid == :uid")
+  String findRefreshTokenByUid(int uid);
+
+  @Query("SELECT token_type FROM UserAuthInfo WHERE uid == :uid")
+  String findTokenTypeByUid(int uid);
+
+  @Query("SELECT token_expires_in FROM UserAuthInfo WHERE uid == :uid")
+  long findTokenExpiresInByUid(int uid);
+
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   void insertUserAuthInfo(UserAuthInfo userAuthInfo);
