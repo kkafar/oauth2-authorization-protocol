b35830e4c3eb4d096dc2f156136c56c93561b550
Kacper Kafara <kacperkafara@gmail.com>
2022-02-19 05:10:23 +0100

    Do not export database schema

--- apps/Fresh/app/src/main/java/com/kkafara/fresh/database/MainDatabase.java
+++ apps/Fresh/app/src/main/java/com/kkafara/fresh/database/MainDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 import com.kkafara.fresh.database.dao.AuthInfoDao;
 import com.kkafara.fresh.database.entity.AuthInfoRecord;
 
-@Database(entities = {AuthInfoRecord.class}, version = 1, exportSchema = true)
+@Database(entities = {AuthInfoRecord.class}, version = 1, exportSchema = false)
 public abstract class MainDatabase extends RoomDatabase {
   public abstract AuthInfoDao getAuthInfoDao();
 }
