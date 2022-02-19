4d0135c293b92332427a3cda1dadd4f0f787fb4f
Kacper Kafara <kacperkafara@gmail.com>
2022-02-19 05:08:02 +0100

    Add DataBaseProvider

--- apps/Fresh/app/src/main/java/com/kkafara/fresh/database/MainDatabase.java
+++ apps/Fresh/app/src/main/java/com/kkafara/fresh/database/MainDatabase.java
@@ -1,11 +1,12 @@
 package com.kkafara.fresh.database;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.kkafara.fresh.database.dao.AuthInfoDao;
 import com.kkafara.fresh.database.entity.AuthInfoRecord;
 
 @Database(entities = {AuthInfoRecord.class}, version = 1, exportSchema = true)
-public abstract class MainDatabase {
+public abstract class MainDatabase extends RoomDatabase {
   public abstract AuthInfoDao getAuthInfoDao();
 }
